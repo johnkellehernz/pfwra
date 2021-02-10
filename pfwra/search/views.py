6d23ecadd9cd2f32f572d91429a74cb367721170
Jordi <jordi.joan@gmail.com>
2021-02-11 07:10:04 +1300

    initial app

---
+++ pfwra/search/views.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+
+from wagtail.core.models import Page
+from wagtail.search.models import Query
+
+
+def search(request):
+    # Search
+    search_query = request.GET.get('q', None)
+    if search_query:
+        search_results = Page.objects.live().search(search_query)
+        query = Query.get(search_query)
+        # Record hit
+        query.add_hit()
+
+    else:
+        search_results = Page.objects.none()
+
+    # Pagination
+    page = request.GET.get('page', 1)
+    paginator = Paginator(search_results, 10)
+    try:
+        search_results = paginator.page(page)
+    except PageNotAnInteger:
+        search_results = paginator.page(1)
+    except EmptyPage:
+        search_results = paginator.page(paginator.num_pages)
+
+    return render(
+        request,
+        'search/search_results.html',
+        {
+            'search_query': search_query,
+            'search_results': search_results,
+        },
+    )
