10ddf4d9175800e745a8fb08f98075a4c5fc0e89
Jordi <jordi.joan@gmail.com>
2021-02-19 07:35:05 +1300

    Changed whole project structure

--- manage.py
+++ manage.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 import os
 import sys
-
-import dotenv
-
+from pathlib import Path
 
 if __name__ == "__main__":
-    dotenv.read_dotenv()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pfwra.settings.dev")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django  # noqa
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
 
-    from django.core.management import execute_from_command_line
+        raise
+
+    # This allows easy placement of apps within the interior
+    # pfwra directory.
+    current_path = Path(__file__).parent.resolve()
+    sys.path.append(str(current_path / "pfwra"))
 
     execute_from_command_line(sys.argv)
--- pfwra/search/views.py
+++ pfwra/search/views.py
@@ -1,25 +1,24 @@
-from django.conf import settings
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
-
 from wagtail.core.models import Page
 from wagtail.search.models import Query
 
 
 def search(request):
+    search_query = request.GET.get("query", None)
+    page = request.GET.get("page", 1)
+
     # Search
-    search_query = request.GET.get('q', None)
     if search_query:
         search_results = Page.objects.live().search(search_query)
         query = Query.get(search_query)
+
         # Record hit
         query.add_hit()
-
     else:
         search_results = Page.objects.none()
 
     # Pagination
-    page = request.GET.get('page', 1)
     paginator = Paginator(search_results, 10)
     try:
         search_results = paginator.page(page)
@@ -30,9 +29,6 @@ def search(request):
 
     return render(
         request,
-        'search/search_results.html',
-        {
-            'search_query': search_query,
-            'search_results': search_results,
-        },
+        "search/search.html",
+        {"search_query": search_query, "search_results": search_results},
     )
